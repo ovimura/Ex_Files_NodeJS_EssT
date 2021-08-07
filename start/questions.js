24c89f7743bb5d3cc7097ad26160dd2729683f9a
ovidiu.mura <ovidiu.mura@pacteraedge.com>
2021-08-07 10:34:27 -0700

    adding solution for chapter 1

---
+++ start/questions.js
@@ -0,0 +1,16 @@
+//process.stdin
+//process.stdout.write("Hello ")
+//process.stdout.write("wrold \n\n\n ")
+
+const questions = [
+    "What is your name?",
+    "What would you rather be doing?",
+    "What is your preferred programming language?"
+]
+
+const ask = (i = 0) => {
+    process.stdout.write(`\n\n\n ${questions[i]}`);
+    process.stdout.write(` > `);
+};
+
+ask();
