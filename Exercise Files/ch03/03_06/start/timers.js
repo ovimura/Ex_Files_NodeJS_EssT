5eb89cb5e2a27c4a22aa0dac4e3cf7812769abe9
ovidiu.mura <ovidiu.mura@pacteraedge.com>
2021-08-09 10:12:13 -0700

    adding exercices for chapter Timers

---
+++ Exercise Files/ch03/03_06/start/timers.js
@@ -0,0 +1,29 @@
+const waitTime = 3000;
+const waitInterval = 500;
+let currentTime = 0;
+
+const incTime = () => {
+    currentTime += waitInterval
+    console.log(`waiting ${currentTime/1000} seconds`);
+}
+
+
+console.log(`setting a ${waitTime/1000} second delay`);
+
+
+const timerFinished = () => {
+    clearInterval(interval);
+    console.log("done");
+}
+
+
+const interval = setInterval(incTime, waitInterval);
+
+// invoke the funcation timerFinished when the time (waitTime) is completed.
+setTimeout(timerFinished, waitTime);
+
+
+
+
+
+
