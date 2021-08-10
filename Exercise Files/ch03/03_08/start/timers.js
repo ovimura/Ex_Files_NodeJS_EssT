196854b0c9a239b25ac6d5d47bddd1566d392adb
ovidiu.mura <ovidiu.mura@pacteraedge.com>
2021-08-10 10:16:16 -0700

    adding solution for chapter 04, Core Modules

--- Exercise Files/ch03/03_08/start/timers.js
+++ Exercise Files/ch03/03_08/start/timers.js
@@ -5,12 +5,17 @@ let currentTime = 0;
 const incTime = () => {
   currentTime += waitInterval;
   const p = Math.floor((currentTime / waitTime) * 100);
+  process.stdout.clearLine();
+  process.stdout.cursorTo(0);
+  process.stdout.write(`waiting ... ${p}%`);
 };
 
 console.log(`setting a ${waitTime / 1000} second delay`);
 
 const timerFinished = () => {
   clearInterval(interval);
+  process.stdout.clearLine();
+  process.stdout.cursorTo(0);
   console.log("done");
 };
 
