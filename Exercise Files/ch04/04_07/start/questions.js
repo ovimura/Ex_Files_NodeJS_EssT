1c651f4933c40b48db92512b86614c25f4829c70
ovidiu.mura <ovidiu.mura@pacteraedge.com>
2021-08-11 10:28:00 -0700

    adding solution for the exercices of chapter 04, section 08

--- Exercise Files/ch04/04_07/start/questions.js
+++ Exercise Files/ch04/04_07/start/questions.js
@@ -6,8 +6,21 @@ const questions = [
   "What are you going to do with node js? "
 ];
 
-collectAnswers(questions, answers => {
+const answerEvents = collectAnswers(questions, answers => {
   console.log("Thank you for your answers. ");
   console.log(answers);
+  process.exit();
+});
+
+answerEvents.on("answer", answer => {
+  console.log(`question answered: ${answer}`);
+});
+
+answerEvents.on("complete", answers => {
+  console.log("Thank you for your answers. ");
+  console.log(answers);
+});
+
+answerEvents("complete", () => {
   process.exit();
 });
